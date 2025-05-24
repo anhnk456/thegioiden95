cc4374920dcc7dadcc874958605ce34ee665e531
DucNguyen <ducnguyenbvtvc@gmail.com>
2025-05-24 21:42:05 +0700

    chuyen huong api

--- src/api/den-led.js
+++ src/api/den-led.js
@@ -1,7 +1,7 @@
 import axiosInstance from '@/config/axios.config';
 import { convertURLParams } from "@/until/format";
 
-const URL = "http://47.129.50.217:8080/";
+const URL = "/api/";
 
 const API = {
   // API Sản phẩm
--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,14 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://the-gioi-den.up.railway.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false
+      }
+    }
+  }
 });
