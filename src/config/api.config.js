6a98fb2a8d644aa038727858263d328005a7432e
DucNguyen <ducnguyenbvtvc@gmail.com>
2025-05-24 22:03:24 +0700

    revert

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL: 'https://the-gioi-den.up.railway.app',
-  API_URL: 'https://the-gioi-den.up.railway.app/api',
-  AUTH_URL: 'https://the-gioi-den.up.railway.app/api/auth'
+  BASE_URL: '/api',
+  API_URL: '/api/api',
+  AUTH_URL: '/api/api/auth'
 }; 
